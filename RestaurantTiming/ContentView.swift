6c73bc5cff1b83891dab7b5e19a7a2ba37924dc7
Ganesh Reddy <annapaganeshreddy@gmail.com>
2024-06-01 20:01:09 -0500

    Initial Commit

---
+++ RestaurantTiming/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RestaurantTiming
+//
+//  Created by Ganesh reddy on 6/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RestaurantTiming/RestaurantTimingApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantTimingApp.swift
+//  RestaurantTiming
+//
+//  Created by Ganesh reddy on 6/1/24.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantTimingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
